d187fa7090f14032ff48f6e88b4eaf775a50dc81
arsh-mhmd <84625026+arsh-mhmd@users.noreply.github.com>
2022-01-23 11:08:34 +0000

    Initial Commit

---
+++ HumanClock/src/test/java/com/example/HumanClock/HumanClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HumanClock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
